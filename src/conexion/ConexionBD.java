428f5dd6b4c402dc1842aae218a065476eac8255
Veronica Molina <veronica04molina@gmail.com>
2026-06-24 15:00:44 -0500

    Configuración conexión JDBC PostgreSQL

--- src/conexion/ConexionBD.java
+++ src/conexion/ConexionBD.java
@@ -1,3 +1,38 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConexionBD {
 
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/db_barberapp";
+
+    private static final String USER = "postgres";
+
+    private static final String PASSWORD = "2008";
+
+    public static Connection conectar() {
+
+        try {
+
+            Connection conexion =
+                    DriverManager.getConnection(
+                            URL,
+                            USER,
+                            PASSWORD
+                    );
+
+            System.out.println("Conexión exitosa a PostgreSQL");
+
+            return conexion;
+
+        } catch (Exception e) {
+
+            System.out.println("Error de conexión: "
+                    + e.getMessage());
+
+            return null;
+        }
+    }
 }
--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,3 +1,12 @@
+package principal;
+
+import conexion.ConexionBD;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        ConexionBD.conectar();
+
+    }
 }
